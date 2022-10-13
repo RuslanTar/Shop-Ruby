3fc24c2d12cd0f4e53849fd81f729db5a682c822
RuslanTar <ruslantar02@gmail.com>
2022-10-13 14:41:05 +0300

    added gem annotate, annotated with default params

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: orders
+#
+#  id          :bigint           not null, primary key
+#  order_items :string
+#  status      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
