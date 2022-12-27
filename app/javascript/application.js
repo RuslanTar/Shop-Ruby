4db94ec72f03c32c2b8c2da6c030a650af0c4624
RuslanTar <ruslantar02@gmail.com>
2022-12-27 14:16:32 +0200

    added darkmode added logo added responsive for modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,59 @@ import 'bootstrap'
 
 // //= require fontawesome/all
 // FontAwesome.config.mutateApproach = 'sync'
+
+document.addEventListener("DOMContentLoaded", () => {
+    switcherState();
+    switchMode(document.getElementById("darkmode-toggle"));
+});
+
+function switcherState() {
+    // Assuming the default color mode is 'light' in Step 1.
+    const matcher = window.matchMedia('(prefers-color-scheme: dark)');
+    if (localStorage.getItem('data-color-mode') === 'dark' ||
+        (matcher.matches && !localStorage.getItem('data-color-mode'))) {
+        document.documentElement.setAttribute('data-color-mode', 'dark');
+        document.getElementsByClassName('color_mode_button')[0].setAttribute('checked', '')
+    }
+}
+
+function switchLight(style) {
+    style.setProperty('--switcher-background', 'var(--bs-light)');
+    style.setProperty('--switcher-moon', '#7e7e7e');
+    style.setProperty('--switcher-sun', 'var(--bs-white)');
+    style.setProperty('--switcher-left', '2px');
+    style.setProperty('--switcher-transform', 'translateX(0)');
+    style.setProperty('--switcher-gradient', 'linear-gradient(180deg,#ffcc89,#d8860b)');
+}
+
+function switchDark(style) {
+    style.setProperty('--switcher-background', 'var(--bs-dark)');
+    style.setProperty('--switcher-moon', 'var(--bs-white)');
+    style.setProperty('--switcher-sun', '#7e7e7e');
+    style.setProperty('--switcher-left', '58px');
+    style.setProperty('--switcher-transform', 'translateX(-100%)');
+    style.setProperty('--switcher-gradient', 'linear-gradient(180deg,#777,#3a3a3a)');
+}
+
+function switchMode(targetElement) {
+    const checked = targetElement.checked;
+    let root_style = document.querySelector(':root').style;
+    if (!checked) {
+        document.documentElement.setAttribute('data-color-mode', 'light');
+        localStorage.setItem('data-color-mode', 'light');
+        switchLight(root_style);
+    } else if (checked) {
+        document.documentElement.setAttribute('data-color-mode', 'dark');
+        localStorage.setItem('data-color-mode', 'dark');
+        switchDark(root_style);
+    } else {
+        throw new Error(`unrecognized color mode button id ${targetElement}.`)
+    }
+}
+
+document.querySelectorAll('.color_mode_button').forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const targetElement = /** @type {HTMLElement} */ (event.currentTarget);
+        switchMode(targetElement);
+    });
+});
